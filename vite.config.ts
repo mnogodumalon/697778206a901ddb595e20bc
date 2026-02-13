37134735c258645b821b39ec8eee61e1da06f9b0
Lilo <lilo@livinglogic.de>
2026-02-13 12:44:48 +0000

    Lilo Auto-Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({ 
-  base: '/697778206a901ddb595e20bc/',
+  base: '/github/697778206a901ddb595e20bc/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
